fc13955cd406e5c41ad316322ff450ed665e24ed
David Cascade <david.cascade@outlook.com>
2024-12-05 19:47:16 -0700

    Base start for daltronic

---
+++ components/Footer.tsx
@@ -0,0 +1,32 @@
+
+
+const sectionStyle = {
+    width: "100%",
+    height: "400px",
+    backgroundImage: "url(/DaltronicFooter.png)",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+};
+
+export default function Footer() {
+    return (
+        <div style={sectionStyle} className="w-full sticky bottom-0">
+            <div className="p-8 flex flex-col justify-between h-3/4">
+                <h2 className="text-4xl">Contact</h2>
+
+                <div>
+                    <div className="text-gray-400">
+                        (720)334-8094
+                    </div>
+                    <div className="text-gray-400">
+                        dallas@daltronic.com
+                    </div>
+                </div>
+
+                <div>
+                    <button className="bg-[#2bb673] p-4 border-md hover:bg-[#009d51]">GET IN TOUCH</button>
+                </div>
+            </div>
+        </div>
+    );
+}
