588417659ae5fdec00624702f97e26be85b33407
David Cascade <david.cascade@outlook.com>
2024-12-06 10:30:28 -0700

    More responsiveness

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,21 +8,21 @@ const sectionStyle = {
 
 export default function Footer() {
     return (
-        <div style={sectionStyle} className="w-full bottom-0 h-[300px] sm:h-[200px]">
-            <div className="p-8 flex flex-col justify-between h-3/4">
+        <div style={sectionStyle} className="w-full bottom-0 h-[200px] lg:h-[300px] xl:h-[300px]">
+            <div className="p-4 flex flex-col justify-between h-3/4">
                 <h2 className="text-4xl">Contact</h2>
 
                 <div>
                     <div className="text-gray-400">
-                        (720)334-8094
+                        (720) 334-8094
                     </div>
                     <div className="text-gray-400">
                         dallas@daltronic.com
                     </div>
                 </div>
 
-                <div>
-                    <button className="bg-[#2bb673] p-4 border-md hover:bg-[#009d51]">GET IN TOUCH</button>
+                <div className="mt-2">
+                    <button className="bg-[#2bb673] p-4 rounded-md hover:bg-[#009d51]">GET IN TOUCH</button>
                 </div>
             </div>
         </div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -7,16 +7,16 @@ const logoContainerStyle = {
 
 export default function Header() {
     return (
-        <div className="p-8">
+        <div className="p-4 lg:p-8 xl:p-8">
             <div className="w-full flex justify-between pb-8">
                 <span className="font-bold py-4">Daltronic LLC</span>
-                <button className="bg-[#2bb673] p-4 border-md hover:bg-[#009d51]">GET IN TOUCH</button>
+                <button className="bg-[#2bb673] p-4 rounded-md hover:bg-[#009d51]">GET IN TOUCH</button>
             </div>
             <div className="w-full flex justify-between flex-wrap">
-                <div className="w-full lg:w-1/2 max-w-[200px] h-[150px]" style={logoContainerStyle}>
+                <div className="w-full text-center lg:w-1/2 max-w-[200px] h-[150px]" style={logoContainerStyle}>
                 </div>
                 <div className="w-full lg:w-1/2">
-                    <div className="text-4xl leading-relaxed text-center lg:text-right xl:text-right">EXPERIENCED AUTOMATION & CONTROLS INTEGRATION</div>
+                    <div className="text-2xl lg:text-4xl xl:text-4xl leading-relaxed text-center lg:text-right xl:text-right">EXPERIENCED AUTOMATION & CONTROLS INTEGRATION</div>
                 </div>
             </div>
         </div>
